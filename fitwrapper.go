af04aaf503012c15e659a83d14c02c01d55e66f2
Craig Prevallet <penguintx@hotmail.com>
2021-09-10 10:47:51 -0500

    Add license information.

--- fitwrapper.go
+++ fitwrapper.go
@@ -1,3 +1,30 @@
+/*
+ * This program provides a GTK graphical user interface to PLPlot graphical
+ * plotting routines in order to plot running files stored in the Garmin
+ * (Dynastream) FIT format.
+ *
+ * License:
+ *
+ * Permission is granted to copy, use, and distribute for any commercial
+ * or noncommercial purpose in accordance with the requirements of
+ * version 2.0 of the GNU General Public license.
+ *
+ * This package is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * On Debian systems, the complete text of the GNU General
+ * Public License can be found in `/usr/share/common-licenses/GPL-2'.
+ *
+ * - Craig S. Prevallet, July, 2020
+ */
+
+
 // Compile with
 // go build -buildmode=c-archive fitwrapper.go
 
