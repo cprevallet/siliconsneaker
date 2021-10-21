6513b90fba2c5762237cd69e96cfd948ed4f365f
Craig Prevallet <penguintx@hotmail.com>
2021-10-21 14:29:10 -0500

    Update deployment files and set new release number.

--- main.c
+++ main.c
@@ -72,7 +72,7 @@
 //
 // Declarations section
 //
-#define VERSION "1.3"
+#define VERSION "1.4"
 // Maximum readable records from a fit file.
 // 2880 is large enough for 4 hour marathon at 5 sec intervals
 //#define NSIZE 2880
