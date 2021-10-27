79c82aaebd92bd7c67cbc93997a82c5f3043a7fc
Craig Prevallet <penguintx@hotmail.com>
2021-10-27 15:37:48 -0500

    Revert.

--- main.c
+++ main.c
@@ -2365,19 +2365,6 @@ main (int argc, char *argv[])
                                  GTK_STYLE_PROVIDER(provider),
                                  GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-  /* Custom styling on the file open to draw attention. */
-  GtkStyleContext *context2 = gtk_widget_get_style_context(GTK_WIDGET(btnFileOpen));
-  GtkCssProvider *provider2 = gtk_css_provider_new();
-  gtk_css_provider_load_from_data(provider2,
-                                      "filechooserbutton>button {background-color: #00AAAA;}\n", -1, NULL);
-  gtk_style_context_add_provider(context2,
-                                 GTK_STYLE_PROVIDER(provider2),
-                                 GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-
-
-
-
   /* Release the builder memory. */
   g_object_unref (builder);
 
